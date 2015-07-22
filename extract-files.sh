02610f2e4a0490a66d13e71dc77b78f19d2b9b4b
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2015-07-22 10:51:32 +0200

    Bug 1186008 - Renaming Z3 KK from shinano to leo-kk r=mwu

--- extract-files.sh
+++ extract-files.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DEVICE=shinano
+DEVICE=leo
 MANUFACTURER=sony
 
 . ../shinano-common/extract-files.sh
